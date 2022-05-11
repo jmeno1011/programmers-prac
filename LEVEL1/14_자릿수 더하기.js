b41dbaa9f81586fa15a0c17e1785923082895551
jmeno1011 <92106615+jmeno1011@users.noreply.github.com>
2022-05-11 15:13:24 +0900

    Create 14_자릿수 더하기.js

---
+++ LEVEL1/14_자릿수 더하기.js
@@ -0,0 +1,12 @@
+// 자연수 N이 주어지면, N의 각 자릿수의 합을 구해서 return 하는 solution 함수를 만들어 주세요.
+// 예를들어 N = 123이면 1 + 2 + 3 = 6을 return 하면 됩니다.
+
+// N / answer
+// 123 / 6
+// 987 / 24
+
+function solution(n)
+{
+    var answer = 0;
+    return String(n).split("").reduce((sum, value)=> (sum + Number(value)),answer)
+}

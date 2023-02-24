fb34989212c192e9528e7bcb1199cf31b575314c
Tono <92106615+jmeno1011@users.noreply.github.com>
2023-02-24 16:08:51 +0900

    [level 0] Title: 직사각형 넓이 구하기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.js
@@ -0,0 +1,16 @@
+function solution(dots) {
+    var answer = 0;
+    dots.sort((a,b)=>{
+        if(a[0] === b[0]){
+            return a[1] > b[1] ? 1 : -1;
+        }else{
+            return a[0] > b[0] ? 1 : -1;
+        }
+    })
+    answer = (
+        dots[1][0] * dots[0][1] + dots[3][0] * dots[1][1] + dots[2][0] * dots[3][1] + 
+        dots[0][0] * dots[2][1] - (dots[0][0] * dots[1][1] + dots[1][0] * dots[3][1] +
+        dots[3][0] * dots[2][1] + dots[2][0] * dots[0][1])
+    ) / 2
+    return answer = Math.abs(answer);
+}
